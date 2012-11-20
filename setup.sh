f86605a0c93d47174388f66f734d156ee705fb92
Michael Floering <michaelfloering@gmail.com>
2012-11-19 23:05:15 -0500

    Rename gnome-brightness-control.sh to bri.sh and change setup.sh accordingly

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 # setup.sh: set up gnome-brightness-control as command 'bri'
 # author: Michael Floering
 
-gksudo ln -s ./gnome-brightness-control.sh /usr/bin/bri"
+gksudo ln -s ./bri.sh /usr/bin/bri"
 
 
 
