17e0bfa4f024f9de663dbec0cd6ce0772ac7723a
Michael Floering <michaelfloering@gmail.com>
2012-11-19 22:23:14 -0500

    Get basic functionalities of main script working

--- gnome-brightness-control.sh
+++ gnome-brightness-control.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# gnome-brightness-control.sh: increment/decrement brightness in gnome
+# gnome-brightness-control.sh: script to change laptop brightness in gnome
 # author: Michael Floering
 
 source $(dirname $0)/config.cfg
@@ -8,35 +8,62 @@ source $(dirname $0)/config.cfg
 ## Initialize ##
 ################
 
-# TODO: ythe thing that stores a value for brightness and does increments/decrements of it. That's the core functionality.
-# PUT THAT IN ANOTHER FILE, THAT PASSES ARGUMENTS TO THIS ONE.
-
-if ! $1; then
-    brightness_percentage=$default
+if [ ! -f "./current-brightness" ] ; then
+    # if we don't have a file, start at default
+    brightness=$default
 else
-    if $1 -gt 100; then
-        brightness_percentage=100
-    elif $1 -lt 0; then
-        brightness_percentage=0
-    elif $1 IS NOT A NUMBER; then
-        brightness_percentage=$default
+    # otherwise read the value from the file
+    brightness=`cat ./current-brightness`
+fi
+
+#################
+## Handle args ##
+#################
+
+### Called without argument; 'reset' brightness to default
+if [ ! "$1" ]; then
+    echo "No percentage; setting to default instead"
+    brightness=$default
+
+### Parse arguments
+else
+    # good: + or -
+    if [[ "$1" =~ ^\+$ ]] ; then
+        echo "Incrementing by $interval percent"
+        brightness=`expr ${brightness} + ${interval}`
+    elif [[ "$1" =~ ^\-$ ]] ; then
+        echo "Decrementing by $interval percent"
+        brightness=`expr ${brightness} - ${interval}`
+    # good: integers (in range 0-100 if they got to this point)
     else
-        brightness_percentage=$1
+        brightness=$1
     fi
 fi
 
-# XXX:
-# There is a function that should  GET brightness:
-# gdbus call --session --dest org.gnome.SettingsDaemon --object-path /org/gnome/SettingsDaemon/Power --method org.gnome.SettingsDaemon.Power.Screen.GetPercentage
-# but for me it always SETS to 100. So currently we do not have a way to GET brightness.
+### Weed out bad values for the argument
+
+# bad: not +/- or an integer
+if ! [[ "$brightness" =~ ^[-]?[0-9]+$ ]] ; then
+    echo "Not an integer; setting to default percentage instead"
+    brightness=$default
+# bad: integers out of the range 0-100
+elif [ "$brightness" -gt 100 ] ; then
+    echo "Greater than one hundred percent; setting to 100% instead"
+    brightness=100
+elif [ "$brightness" -lt 0 ] ; then
+    echo "Less than one hundred percent; setting to 0% instead"
+    brightness=0
+fi
+
+# save setting for next time
+echo "${brightness}" > ./current-brightness
 
 ###################
 ## Main function ##
 ###################
 
 # source that pointed me to this command: http://askubuntu.com/questions/178686/can-i-set-superuser-inside-the-launcher
-gdbus call --session --dest org.gnome.SettingsDaemon --object-path /org/gnome/SettingsDaemon/Power --method org.gnome.SettingsDaemon.Power.Screen.SetPercentage $brightness_percentage
-
+gdbus call --session --dest org.gnome.SettingsDaemon --object-path /org/gnome/SettingsDaemon/Power --method org.gnome.SettingsDaemon.Power.Screen.SetPercentage $brightness
 
 ##########################
 ## Possible functions ? ##
@@ -44,6 +71,8 @@ gdbus call --session --dest org.gnome.SettingsDaemon --object-path /org/gnome/Se
 
 # Here is somebody's script to change brightness on plugging in the power vs. battery ... 
 # http://askubuntu.com/questions/93601/change-screen-brightness-automatically-on-ac-connection-disconnection
+
+
 
 
 
