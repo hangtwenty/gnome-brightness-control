dcd95bba3fe6002494b2c3cd84a78dde494bb7e2
Michael Floering <michaelfloering@gmail.com>
2012-12-11 16:01:59 -0500

    Make setup.sh put a link to the bri script in /etc/pm/sleep.d/ instead of copy.
    Less redundant this way.

--- setup.sh
+++ setup.sh
@@ -3,9 +3,9 @@
 # author: Michael Floering
 
 # factor out some literals
-bri='bri'
-_bri='.bri'
-bri_sh='bri.sh'
+bri='bri' # the command name `bri`
+_bri='.bri' # the name of the configuration file
+bri_sh='bri.sh' # the original name of the shell script, in this repository.
 
 # make sure bri.sh is executable
 chmod +x $bri_sh
@@ -13,14 +13,15 @@ chmod +x $bri_sh
 # copy .bri (config file) to ~/.bri
 cp ./$_bri ~/$_bri
 
-# copy bri.sh to /usr/bin/ so that it's a command 'bri',
+# copy bri.sh to /usr/bin/bri so that it's a command 'bri' ...
 cp ./$bri_sh /usr/bin/$bri
-# and also copy it to /etc/pm/sleep.d/ so it is called on 'thaw' and 'resume'
-# (from hiberation and suspend, respectively)
-cp ./$bri_sh /etc/pm/sleep.d/$bri
+
+# ... and also make a link to this script in /etc/pm/sleep.d/
+# (all scripts in /etc/pm/sleep.d/ are called upon thaw/resume on waking
+# from hiberation and suspend, respectively)
+ln -s /usr/bin/$bri /etc/pm/sleep.d/$bri
 
 exit 0
-
 
 
 
