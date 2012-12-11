85ae93af6dd619f05507b910c090920102251ed6
Michael Floering <michaelfloering@gmail.com>
2012-12-11 14:58:53 -0500

    Heavily revise (nearly rewrite) bri.sh to make it the cleanest version yet.
    The code in bri.sh is now, in my opinion, at its most readable, while still
    implementing all functionality - including the newly implemented configuration
    setting (`$bri_persist`).
    
    Next step before 0.1 release is just to finish the README, then we should be
    good to go.

--- bri.sh
+++ bri.sh
@@ -22,43 +22,72 @@ fi
 ## Handle args ##
 #################
 
-### Got argument; parse.
-if [ "$1" ]; then
-    case "$1" in
-        +) # increment
-            echo "Incrementing by $bri_interval percent"
-            brightness=`expr ${brightness} + ${bri_interval}`
-            ;;
-        -) # decrement
-            echo "Decrementing by $bri_interval percent"
-            brightness=`expr ${brightness} - ${bri_interval}`
-            ;;
-        [0-9]) # is an integer
-            # reset integers out of the range 0-100 to min/max of that range
-            if [ "$1" -gt 100 ] ; then
-                echo "Greater than one hundred percent; setting to 100% instead"
-                brightness=100
-            elif [ "$1" -lt 0 ] ; then
-                echo "Less than one hundred percent; setting to 0% instead"
-                brightness=0
-            fi
-            ;;
-        thaw|resume)
-            # bri is being run on returning from hibernation/suspend.
-            # (on Ubuntu scripts placed /etc/pm/sleep.d/ are 
-            # run as root-user with args "thaw" or "resume",
-            # upon waking from hiberation/suspend.
-            brightness=$bri_default
-            ;;
-        *) # does not match any accepted argument
-            echo "Not an integer; setting to default percentage instead"
-            brightness=$bri_default
-            ;;
-    esac
-elif [ ! "$1" ]; then
-    ### Called without argument; 'reset' brightness to default
-    echo "No percentage; setting to default instead"
-    brightness=$bri_default
+### Test that there is an argument, or set to a no-op argument if there is none.
+if [ ! "$1" ]; then
+    # if no argument, set $bri_arg to this no-op string.
+    bri_arg="not_a_number"
+    # Explanation for this:
+    #   `case` doesn't seem to have a good way to catch an empty string.
+    #   this catches an empty string / absense of argument and rewrites it
+    #   as a "no-op" string / argument. the reason for this is that we need
+    #   to catch non-number arguments before we get to the number-comparison
+    #   that happens after the case statement.
+else
+    bri_arg="$1"
+fi
+
+### Interpret argument
+case "$bri_arg" in
+    *[!0-9]* ) # is NOT a string of digits
+        # so this nested case statement processes all non-digit arguments.
+        case "$bri_arg" in
+            + ) # increment
+                echo "Incrementing by $bri_interval percent"
+                brightness=`expr ${brightness} + ${bri_interval}`
+                ;;
+            - ) # decrement
+                echo "Decrementing by $bri_interval percent"
+                brightness=`expr ${brightness} - ${bri_interval}`
+                ;;
+            thaw|resume|* ) # explicit is better than implicit :)
+                # *
+                #   no (known) argument.
+                # thaw|resume
+                #   bri is being run on returning from hibernation/suspend.
+                #   (on Ubuntu scripts placed /etc/pm/sleep.d/ are 
+                #   run as root-user with args "thaw" or "resume",
+                #   upon waking from hiberation/suspend.
+                echo "Bare invocation. (No arg, bad arg, or arg was thaw/resume.)"
+                if [ ! $bri_persist ]; then
+                    echo "Setting to default brightness."
+                    brightness=$bri_default
+                else
+                    echo "Setting to last brightness."
+                # brightness already == $current_brightness_file from earlier.
+                fi
+                ;;
+        esac
+        ;;
+    *)  # alright, then it must be a string of digits
+        # (this also catches an empty string, but that was weeded out above)
+        brightness=$1
+        ;;
+esac
+
+# double-check brightness to avoid errors;
+# reset integers out of the range 0-100 to 0/100 of that range
+if [[ "$brightness" =~ ^[-]?[0-9]+$ ]] ; then
+    if [ "$brightness" -gt 100 ] ; then
+        echo "Greater than one hundred percent; setting to 100% instead"
+        brightness=100
+    elif [ "$brightness" -lt 0 ] ; then
+        echo "Less than one hundred percent; setting to 0% instead"
+        brightness=0
+    fi
+else
+    echo "For some reason, brightness is $brightness and is not a number."
+    echo "Exiting with status 1"
+    exit 1
 fi
 
 # save setting for next time
