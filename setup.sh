22b584949ef9a2befbfe13b5f099946ad0c865ca
Michael Floering <michaelfloering@gmail.com>
2012-12-11 13:32:11 -0500

    Add (experimental) support for setting brightness on thaw/resume.
    I can confirm that running `bri thaw` or `bri resume` will do the trick,
    but from a quick test it doesn't seem like it's working on my laptop.
    This may be because the computer isn't really "suspended" (I'm just closing it
    and reopening it, only waiting about a minute). Maybe it needs to be closed
    for longer for it to count as suspension and for this to work out right.
    
    Additionally, I revised part of bri.sh, in an attempt to refactor it.

--- setup.sh
+++ setup.sh
@@ -2,14 +2,24 @@
 # setup.sh: set up gnome-brightness-control as command 'bri'
 # author: Michael Floering
 
+# factor out some literals
+bri='bri'
+_bri='.bri'
+bri_sh='bri.sh'
+
 # make sure bri.sh is executable
-chmod +x bri.sh
+chmod +x $bri_sh
 
 # copy .bri (config file) to ~/.bri
-cp ./.bri ~/.bri
+cp ./$_bri ~/$_bri
 
-# copy bri.sh to /usr/bin/ so that it's a command 'bri'
-gksudo cp ./bri.sh /usr/bin/bri && exit 0
+# copy bri.sh to /usr/bin/ so that it's a command 'bri',
+cp ./$bri_sh /usr/bin/$bri
+# and also copy it to /etc/pm/sleep.d/ so it is called on 'thaw' and 'resume'
+# (from hiberation and suspend, respectively)
+cp ./$bri_sh /etc/pm/sleep.d/$bri
+
+exit 0
 
 
 
